76968c473f40cd4c4e4fd267ef76efd1cd4a3234
Hulda Lilja <huldah15@ru.is>
2021-01-17 18:27:28 +0100

    different background color for dark mode

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f9f271e52fc60f22f787b04830a556e2.js"
+  "/precache-manifest.3deebd4c64b08c11b7bc873d17febecf.js"
 );
 
 self.addEventListener('message', (event) => {
